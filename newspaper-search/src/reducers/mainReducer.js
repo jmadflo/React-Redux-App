43d6a4dd36c54f2eec7a01a589e2f1824854d67c
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-04-15 13:38:50 -0700

    basic redux setup

---
+++ newspaper-search/src/reducers/mainReducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    data: [],
+    searchInput: '',
+    searchType: ''
+}
+
+export const mainReducer = (state = initialState, actions) => {
+    switch(actions.type) {
+        default:
+            return state
+    }
+}
